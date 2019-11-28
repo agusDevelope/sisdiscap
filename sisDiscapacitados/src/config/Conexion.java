132e80904225b2335991fbf70e149387b8990888
agusDevelope <renegadodesade.120@gmail.com>
2019-11-28 10:31:10 -0600

    Proyecto

---
+++ sisDiscapacitados/src/config/Conexion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author AGUS OMEN
+ */
+public class Conexion {
+     Connection con;
+     
+     public Conexion(){
+         
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sisepd","root","");
+         } catch (Exception e) {
+         }
+         
+     }
+     public Connection getConnection(){
+        return con;
+    }
+}
---
+++ sisepd.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-11-2019 a las 07:57:09
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.3.1
+CREATE database sisepd;
+use sisepd;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sisepd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE `admin` (
+  `ID` int(11) NOT NULL,
+  `NOMBRE` varchar(100) CHARACTER SET utf32 COLLATE utf32_spanish2_ci NOT NULL,
+  `PASS` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `discapacitados`
+--
+
+CREATE TABLE `discapacitados` (
+  `PLACA` varchar(10) NOT NULL,
+  `NOMBRE` varchar(150) NOT NULL,
+  `DIRECCION` varchar(200) NOT NULL,
+  `TELEFONO` varchar(10) NOT NULL,
+  `NUM_TARJETA` varchar(20) NOT NULL,
+  `FEC_NAC` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estacionamiento`
+--
+
+CREATE TABLE `estacionamiento` (
+  `ID_EST` int(11) NOT NULL,
+  `PROPIETARIO` varchar(150) NOT NULL,
+  `DIRECCION` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `ID_REG` int(11) NOT NULL,
+  `HR_LLEGADA` time NOT NULL,
+  `HR_SALIDA` time NOT NULL,
+  `FECHA` date NOT NULL,
+  `ID_EST` int(11) NOT NULL,
+  `LUGAR` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `discapacitados`
+--
+ALTER TABLE `discapacitados`
+  ADD PRIMARY KEY (`PLACA`);
+
+--
+-- Indices de la tabla `estacionamiento`
+--
+ALTER TABLE `estacionamiento`
+  ADD PRIMARY KEY (`ID_EST`),
+  ADD KEY `ID_EST` (`ID_EST`);
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`ID_REG`),
+  ADD KEY `ID_EST` (`ID_EST`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estacionamiento`
+--
+ALTER TABLE `estacionamiento`
+  MODIFY `ID_EST` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `ID_REG` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD CONSTRAINT `registro_ibfk_1` FOREIGN KEY (`ID_EST`) REFERENCES `estacionamiento` (`ID_EST`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
